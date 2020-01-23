f1086f50543fe655eed2dc1c05e0d21615dca412
Jeremy Quick <jrquick@uab.edu>
2020-01-23 09:07:03 -0600

    Add auto complete to the header

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import {AutoCompleteModule} from 'ionic4-auto-complete';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
+    AutoCompleteModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
